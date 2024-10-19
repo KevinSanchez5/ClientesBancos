0478801d1e0b227b1602db34dd36ec4c34834b44
Diego NL <diegonoviluce28@gmail.com>
2024-10-19 13:04:58 +0200

    Añadp la cache y los archivos para desplegar la aplicación en docker

--- src/main/java/banco/Main.java
+++ src/main/java/banco/Main.java
@@ -1,11 +1,85 @@
 package banco;
 
 
+import banco.data.local.LocalDatabaseConfig;
+import banco.data.local.LocalDatabaseInitializer;
+import banco.data.local.LocalDatabaseManager;
+import banco.data.remote.RemoteDatabaseManager;
+import banco.domain.cards.model.BankCard;
+import banco.domain.cards.repository.BankCardRepositoryImpl;
+import banco.domain.clients.model.Client;
+import banco.domain.clients.repository.ImplClientRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            LocalDatabaseManager manager = LocalDatabaseManager.getInstance();
+            LocalDatabaseConfig config = new LocalDatabaseConfig("localclients/database.properties");
+            LocalDatabaseInitializer initializer = new LocalDatabaseInitializer(config, manager);
+            initializer.initializeDatabase();
+
+            ImplClientRepository clientRepository = ImplClientRepository.getInstance(manager);
+
+            // guardar cliente
+            Client newClient = new Client(null, "Jane Doe", "janedoe", "jane.doe@example.com", new ArrayList<>(), null, null);
+            clientRepository.save(newClient).thenAccept(client -> {
+                System.out.println("Cliente guardado: " + client);
+            });
+
+            Thread.sleep(1500);
+
+            clientRepository.findById(1L).thenAccept(client -> {
+                System.out.println("Cliente encontrado: " + client);
+            });
+
+            //Thread.sleep(2000);
 
 
+            Client updatedClient = new Client(1L, "Jane Smith", "janesmith", "jane.smith@example.com", new ArrayList<>(), null, null);
+            clientRepository.update(1L, updatedClient).thenRun(() -> {
+                System.out.println("Cliente actualizado.");
+            });
 
+            Thread.sleep(2000);
+
+            clientRepository.findAll().thenAccept(clients -> {
+                System.out.println("Clientes en la base de datos: ");
+                clients.forEach(System.out::println);
+            });
+
+            clientRepository.delete(1L).thenRun(() -> {
+                System.out.println("Cliente eliminado.");
+            });
+
+            Thread.sleep(2000);
+
+            clientRepository.findAll().thenAccept(clients -> {
+                System.out.println("Clientes en la base de datos: ");
+                clients.forEach(System.out::println);
+            });
+
+            RemoteDatabaseManager db= RemoteDatabaseManager.getInstance();
+            BankCardRepositoryImpl bankCardRepository = BankCardRepositoryImpl.getInstance(db);
+
+            bankCardRepository.save(
+                    BankCard.builder()
+                            .number("9876543210987654")
+                            .clientId(1L)
+                            .expirationDate(LocalDate.now().plusYears(3))
+                            .createdAt(LocalDateTime.now())
+                            .updatedAt(LocalDateTime.now())
+                            .build()
+            );
+
+            System.out.println(bankCardRepository.findById("9876543210987654").get().toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
---
+++ src/main/java/banco/domain/clients/service/cache/ClientesCache.java
@@ -0,0 +1,6 @@
+package banco.domain.clients.service.cache;
+
+import banco.domain.clients.model.Client;
+
+public interface ClientesCache extends Cache<Long, Client> {
+}

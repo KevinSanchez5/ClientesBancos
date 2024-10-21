a094c4ce0abfd982bd8e074bdab7739d2b3a215b
TomasVaquerin <vaquerinjimenez@gmail.com>
2024-10-21 15:47:39 +0200

    constructor

--- src/main/java/banco/domain/cards/model/BankCard.java
+++ src/main/java/banco/domain/cards/model/BankCard.java
@@ -3,6 +3,7 @@ package banco.domain.cards.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 @Builder
 @Data
 @AllArgsConstructor
+
 public class BankCard {
     private String number;
     private Long clientId;
@@ -18,5 +20,11 @@ public class BankCard {
     private LocalDateTime updatedAt;
 
     public BankCard() {
+    }
+
+    public BankCard(String number, Long clientId, LocalDate expirationDate) {
+        this.number = number;
+        this.clientId = clientId;
+        this.expirationDate = expirationDate;
     }
 }
